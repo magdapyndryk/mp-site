4586729a8c80d9c16295db330a73a76f61df0674
magdapyndryk <magda.pyndryk@dfo.no>
2024-11-01 21:23:04 +0100

    Configure frontend

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header>
+      <h1>My site</h1>
+    </header>
+    <main>
+      <NuxtPage /> <!-- Component using pages from catalog pages/ -->
+    </main>
+    <footer>
+      <p>© 2024 My site</p>
+    </footer>
+  </div>
+</template>
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://127.0.0.1:3000/api',
+    }
+  }
+})
