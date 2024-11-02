263ccd9c2f314b2e933218f75b39a1b44a1af915
magdapyndryk <magda.pyndryk@dfo.no>
2024-11-02 12:33:43 +0100

    Add products management on frontend side

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
-    <header>
-      <h1>My site</h1>
-    </header>
-    <main>
-      <NuxtPage /> <!-- Component using pages from catalog pages/ -->
-    </main>
-    <footer>
-      <p>© 2024 My site</p>
-    </footer>
-  </div>
+  <header>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/products">Products</NuxtLink>
+    </nav>
+  </header>
+  <main>
+    <NuxtPage />
+  </main>
+  <footer>
+    <p>© 2024 My site</p>
+  </footer>
 </template>
+
